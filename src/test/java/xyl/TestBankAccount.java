5d8ab4be6c8254883c965d967fa9be0b26f4e4ff
YinleXu <xyl0815888@gmail.com>
2016-08-08 18:03:22 +0100

    now using 'import static org.junit.Assert.*

--- src/test/java/xyl/TestBankAccount.java
+++ src/test/java/xyl/TestBankAccount.java
@@ -1,6 +1,6 @@
 package xyl;
 
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,6 +10,6 @@ public class TestBankAccount {
   public void testDebitWithSufficientFunds() {
     BankAccount account = new BankAccount(10);
     double amount = account.debit(5);
-    Assert.assertEquals(5.0, amount);
+    assertEquals(5.0, amount);
   }
 }
