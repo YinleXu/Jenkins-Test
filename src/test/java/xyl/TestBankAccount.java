4c50c9de28ed79eab944d9928f5267b0265dd697
YinleXu <xyl0815888@gmail.com>
2016-08-09 00:51:11 +0100

    added test case for insufficient fund scenario

--- src/test/java/xyl/TestBankAccount.java
+++ src/test/java/xyl/TestBankAccount.java
@@ -9,7 +9,14 @@ public class TestBankAccount {
   @Test
   public void testDebitWithSufficientFunds() {
     BankAccount account = new BankAccount(10);
-    double amount = account.debit(5);
-    assertEquals(5.0, amount, 0);
+    double amount = account.debit(4);
+    assertEquals(4.0, amount, 0);
+  }
+  
+  @Test
+  public void testDebitWithInsufficientFunds() {
+      BankAccount account = new BankAccount(10);
+      double amount = amount.debit(15);
+      Assert.assertEquals(10.0,amount,0);
   }
 }
